f12e867b805cd2e86662d843d5c590ef02d8fd44
izsi-salmon <izsi.salmon@gmail.com>
2019-08-29 15:08:42 +1200

    completed nav menu

--- header.php
+++ header.php
@@ -11,9 +11,34 @@
         $custom_logo = get_theme_mod('custom_logo');
         $logo_url = wp_get_attachment_image_url($custom_logo, 'small');
     ?>
+    
+   <div class="header header-mobile">
+       <div class="nav">
+           <?php if ( has_nav_menu( 'header_nav' ) ) {
+                 wp_nav_menu( array(
+                    'theme_location' => 'header_nav'
+                 ) );
+            } ?>
+        </div>
+        <?php if ($custom_logo): ?>
+            <div class="image-logo"><a href="<?= get_site_url(); ?>"><img src="<?= $logo_url ?>" alt="Logo" class="logo"></a></div>
+        <?php else: ?>
+            <div class="text-logo"><?= get_bloginfo('name'); ?></div>
+        <?php endif; ?>
+   </div>
    
-    <?php if ($custom_logo): ?>
-        <div class="image-logo"><a href="<?= get_site_url(); ?>"><img src="<?= $logo_url ?>" alt="Logo" class="logo"></a></div>
-    <?php else: ?>
-        <div class="text-logo"><?= get_bloginfo('name'); ?></div>
-    <?php endif; ?>
+   <div class="header header-desktop">
+       <?php if ($custom_logo): ?>
+            <div class="image-logo"><a href="<?= get_site_url(); ?>"><img src="<?= $logo_url ?>" alt="Logo" class="logo"></a></div>
+        <?php else: ?>
+            <div class="text-logo"><?= get_bloginfo('name'); ?></div>
+        <?php endif; ?>
+        <div class="nav">
+           <?php if ( has_nav_menu( 'header_nav' ) ) {
+                 wp_nav_menu( array(
+                    'theme_location' => 'header_nav'
+                 ) );
+            } ?>
+        </div>
+   </div>
+    
