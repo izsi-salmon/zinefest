9764c37ed226a8f027098c7f2bd05b883228880f
izsi-salmon <izsi.salmon@gmail.com>
2019-08-30 22:19:43 +1200

    updated maincss functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function addCustomThemeStyles(){
   // Style
   wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Nunito:400,700', array(), '0.0.1', 'all');
   wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array(), '5.7.2', 'all');
-  wp_enqueue_style('maincss', get_template_directory_uri().'/assets/css/theme-styles.css', array(), '0.0.9', 'all');
+  wp_enqueue_style('maincss', get_template_directory_uri().'/assets/css/theme-styles.css', array(), '0.1.9', 'all');
 //  wp_enqueue_style('CustomFieldStyle', get_template_directory_uri() . '/assets/custom-fields-styles.css', array(), '1.0.0', 'all');
 //  wp_enqueue_style('commentstyles', get_template_directory_uri() . '/assets/comments-styles.css', array(), '1.0.0', 'all');
   // Scripts
